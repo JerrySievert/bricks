70d460def9bdf15942c6cbe333baf56343486a4f
Jerry Sievert <code@legitimatesounding.com>
2012-01-16 14:03:08 -0800

    switch from date-utils to cromagjs, eliminate monkey-patching

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('date-utils');
+var Cromag = require('cromag');
 
 var app = require('./lib/appserver');
 
 var appServer = new app.appserver();
 
-appServer.addRoute(/.+/, appServer.plugins.sessionhandler, { cookie: { expires: new Date().addYears(1), httpOnly: false } });
+appServer.addRoute(/.+/, appServer.plugins.sessionhandler, { cookie: { expires: new Cromag().addYears(1), httpOnly: false } });
 appServer.addRoute(/.+/, appServer.plugins.filehandler, { basedir: '.' });
 appServer.addRoute(/.+/, appServer.plugins.fourohfour);
 var server = appServer.createServer();
